df5b056dd5ea676aaa1ba71bb246293db6b2e899
Aysheh Alshishani <ayshehalshishani@AyshehsLaptop2.attlocal.net>
2023-06-03 13:34:35 -0400

    installed packages and created a server running on the 8000 port

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
+
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData ={};
+ // Express to run server and routes
+const express = require('express');
 
-// Require Express to run server and routes
+// start up an instance of app
+const app = express();
 
-// Start up an instance of app
-
-/* Middleware*/
+/*Dependencies*/
+const bodyParser = require('body-parser')
+/*Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
+const port = 8000;
+//Spin up the server
+const server = app.listen(port, listening);
+//const server = app.listen(port, ()=>{console.log(`running on localhost: {$port}`)});
+//Call back to debug
+function listening(){
+    console.log("server running"); 
+    console.log(`running on localhost: ${port}`);
+}
 
-// Setup Server
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+
+// Post Route
