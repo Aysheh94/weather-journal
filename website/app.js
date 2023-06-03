2a25804b3fdbda7467aa4e639a90824d9af0e279
Aysheh Alshishani <ayshehalshishani@AyshehsLaptop2.attlocal.net>
2023-06-03 14:13:05 -0400

    created api credential in app.js

--- website/app.js
+++ website/app.js
@@ -3,3 +3,19 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const apikey = "4a018f6bf09a33aba82d8e514eb397dd&units=imperial";
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip='
+
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
